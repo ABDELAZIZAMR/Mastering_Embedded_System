58930b594cc0a56edfed2825523ef4d148ddf489
ABDELAZIZAMR <abdelazeezamr456@gmail.com>
2024-06-27 03:44:19 +0300

    Add Unit3

---
+++ c/Unit3_C_Embedded/lesson 1/Togel_Led/Core/Src/platform.h
@@ -0,0 +1,33 @@
+#ifndef PLATFORM_TYPES_H
+#define PLATFORM_TYPES_H
+
+#include<stdio.h>
+#include<stdint.h>
+
+typedef int8_t sint8;
+typedef unsigned char uint8;
+typedef unsigned char boolean;
+
+typedef int8_t   sint8;
+typedef uint8_t  uint8;
+
+typedef int16_t sint16;
+typedef uint16_t uint16;
+
+typedef int8_t sint32;
+typedef uint32_t uint32;
+
+typedef signed long long sint64;
+typedef unsigned long long uint64;
+
+typedef int_least8_t    sint8_least;
+typedef int_least16_t   sint16_least;
+typedef int_least32_t   sint32_least;
+typedef uint_least8_t   uint8_least;
+typedef uint_least16_t  uint16_least;
+typedef uint_least32_t  uint32_least;
+
+typedef float float32;
+typedef double float64;
+
+#endif
